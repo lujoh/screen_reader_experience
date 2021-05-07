37f79e6cb893ad6975b4c16379c1bbbe68674b84
lujoh <lucyjohnson1995@gmail.com>
2021-05-06 21:39:33 -0400

    fixed bugs

--- access.js
+++ access.js
@@ -13,6 +13,23 @@ function toggle_menu(button, menuID) {
 }
 
 //function to show an alert when the form is submitted
-function form_alert() {
+function form_alert(button) {
     document.getElementById('formresult').innerHTML = "Thank you for your support!";
+    document.getElementById("submit").setAttribute("aria-pressed", "true");
+    return false;
+}
+
+//function to make page content visible/invisible
+function invisible_mode(){
+    var btn = document.getElementById('invisibility_btn');
+    var page = document.getElementById('visible');
+    if (btn.getAttribute("aria-pressed") == "true") {
+        page.className = "";
+        btn.setAttribute("aria-pressed", "false");
+        btn.innerHTML = "Click to explore the page in invisible mode.";
+    } else {
+        page.className = "invisible";
+        btn.setAttribute("aria-pressed", "true");
+        btn.innerHTML = "Click to return to visible mode.";
+    }
 }
